894b767a294a5983938761104f9116e74f54e9a3
Jacobo Flores Perez <144180440+JacoboFlores@users.noreply.github.com>
2023-09-26 07:27:02 -0600

    Add files via upload

---
+++ P12_1s21.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P12_1S21{
+	public static void main(String[] args) {
+		int numero1 = 18;
+		int numero2 = 3;
+
+		int suma = numero1 + numero2;
+		int resta = numero1 - numero2;
+		int multipicacion = numero1 * numero2;
+		double divicion = (double)numero1 / (double)numero2;
+
+		System.out.println(" El resultado de la suma es: " + suma + "\nLa multiplicacion es:" + multipilcacion + "\nLa divicion es: " + divicion)
+	}
+}
